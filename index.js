b49a4e077f7bc02571a660fb9f98a9ff197d1113
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-13 12:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(itemName) {
   for(var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === itemName) {
-      return cart.splice(item);
+      return cart.splice(i, 1);
     }
   }
   return 'That item is not in your cart.';
