e211304ed4f4c8d643af878ce055dc7de8771d7d
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-13 13:04:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,5 +56,6 @@ function removeFromCart(itemName) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === 0) {return "Sorry, we don't have a credit card on file for you."}
-  else (cardNumber < 0) {let cart === 0; return `Your total cost is ` + total + `, which will be charged to the card ${cardNumber}.`
+  else if(cardNumber < 0) {cart.splice(i, i); return `Your total cost is ` + total + `, which will be charged to the card ${cardNumber}.`
+}
 }
